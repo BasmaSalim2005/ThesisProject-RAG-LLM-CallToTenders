802e7b339db75b8f08259e0bfaa20bf1dfc1c641
basma <bsalim@constructor.university>
2026-04-30 14:57:45 +0200

    exampletrying

--- step1/extract.py
+++ step1/extract.py
@@ -10,20 +10,21 @@ client = anthropic.Anthropic(api_key=os.getenv("CLAUDE_API_KEY"))
 
 # The stable 2026 model
 MODEL_ID = "claude-sonnet-4-6"
-FILE_PATH = "step1/Appel-doffre-N30-ANP-2025.pdf"
+FILE_PATH = "RAG/exampe1/RC.pdf"
 
 def extract_all_at_once(file_path):
     with open(file_path, "rb") as f:
         pdf_base64 = base64.b64encode(f.read()).decode("utf-8")
 
     # This prompt tells Claude exactly how to structure the "Master JSON"
+    #"specifications": From the tender document below, extract ONLY project specifications. Return JSON: { "PERIMETRE_DU_PROJET": "", "DEROULEMENT_DU_PROJET": "", "CONSISTANCE_DES_PRESTATIONS": "" } \n\nDocument: ,
+    #"evaluation": From the tender document below, extract ONLY evaluation criteria. Return JSON: { "EVALUATION_DES_OFFRES_DES_CONCURRENTS": { "evaluation_technique": [], "evaluation_financiere": [] } } \n\nDocument: 
     master_prompt = """
     Analyze the attached Moroccan tender PDF and extract three distinct sections.
     Return the result ONLY as a single valid JSON object with exactly these three keys:
     
     "requirements": From the tender document below, extract ONLY the submission document requirements and the publication date of the announcement which is the refference for validiy date of the documents (as a checklist of documents to use for a matching with my db documents) include special cases. Return JSON: { "dossier_administratif": [], "offre_technique": [], "offre_financiere": {} } \n\nDocument:  ,
-    "specifications": From the tender document below, extract ONLY project specifications. Return JSON: { "PERIMETRE_DU_PROJET": "", "DEROULEMENT_DU_PROJET": "", "CONSISTANCE_DES_PRESTATIONS": "" } \n\nDocument: ,
-    "evaluation": From the tender document below, extract ONLY evaluation criteria. Return JSON: { "EVALUATION_DES_OFFRES_DES_CONCURRENTS": { "evaluation_technique": [], "evaluation_financiere": [] } } \n\nDocument: 
+    
     
     Do not include any conversational text or markdown explanation.
     """
